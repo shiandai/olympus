961ac357d56724ce7a53de17943f8556ceebe01b
OwenTsai <shiandai@gmail.com>
2020-07-14 23:55:20 +0800

    Initial Commit

---
+++ olympus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  olympus
+//
+//  Created by Owen on 2020/7/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
